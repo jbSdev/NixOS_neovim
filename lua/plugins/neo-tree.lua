87733b0adc4c898ab737c0197a00f3ed4e8889f5
jbSdev <antoni.jesien@gmail.com>
2024-10-21 23:38:27 +0200

    small neo-tree change
    rounded popup
    skipping 'zsh' buffer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,10 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        require("neo-tree").setup({
+            popup_border_style = "rounded",
+            open_files_do_not_replace_types = {"zsh"}
+        })
         vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', {desc = "Neotree", silent = true})
     end
 }
