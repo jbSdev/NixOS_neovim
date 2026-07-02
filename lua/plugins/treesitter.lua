19b6f895254e17fa56f0d69038f6216872415378
jbSdev <antoni.jesien@gmail.com>
2026-07-03 01:00:02 +0200

    fix: update treesitter config for 0.10 API, remove lazy-lock.json

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,12 @@
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = { "lua", "c", "cpp", "javascript", "html", "python" },
-    highlight   = { enable = true },
-    indent      = { enable = true }
- })
+-- Parsers are Nix-managed; Neovim 0.10+ auto-enables highlighting
+-- Enable treesitter-based indentation for supported filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+        "lua", "c", "cpp", "python", "rust", "go",
+        "javascript", "typescript", "html", "css",
+        "nix", "bash", "json", "yaml", "toml",
+    },
+    callback = function()
+        vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+    end,
+})
